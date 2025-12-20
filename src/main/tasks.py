a3f458f08abb6e486978c3cadbdd85f335099f10
KuthikBohdan1 <bkuzik14@gmail.com>
2025-12-20 23:36:30 +0200

    додав folwer та відкладний запуск

--- src/main/tasks.py
+++ src/main/tasks.py
@@ -9,3 +9,14 @@ from django.core.mail import send_mail
 def send_spam_email(user_email, text):
     send(user_email = user_email, text=text)
     return f"True +{user_email}"
+
+@app.task()
+def my_sum(a, b):
+    return a + b
+
+@app.task(bind=True, default_retry_delay= 5 * 60)
+def task_retry(self, x, y):
+    try:
+        return x + y
+    except Exception as exc:
+        raise self.retry(exc=exc, countdown=60)
